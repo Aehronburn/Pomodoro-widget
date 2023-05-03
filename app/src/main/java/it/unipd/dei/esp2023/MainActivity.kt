3ff548d0e539dc341aaf6b22974f7c20430ccb7c
yihuizhu <yihuozhu@gmail.com>
2023-05-03 21:52:12 +0200

    the window occupied the while screen: it invades status bar and navigation bar spaces. A padding on the top is set in order for the content of fragments to not be hidden behind the status bar

--- app/src/main/java/it/unipd/dei/esp2023/MainActivity.kt
+++ app/src/main/java/it/unipd/dei/esp2023/MainActivity.kt
@@ -2,29 +2,33 @@ package it.unipd.dei.esp2023
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.ViewGroup.MarginLayoutParams
+import androidx.core.view.*
+import androidx.core.view.ViewCompat.setOnApplyWindowInsetsListener
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.navigation.NavigationBarView
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val bottomNavigationBarView = findViewById<NavigationBarView>(R.id.bottomNavigationView)
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+
         val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
         val navController = navHostFragment.navController
+        val bottomNavigationBarView = findViewById<NavigationBarView>(R.id.bottomNavigationView)
         bottomNavigationBarView.setupWithNavController(navController)
-/*
-        val appBarConfiguration = AppBarConfiguration(
-            setOf(
-                R.id.navigation_home,
-                R.id.navigation_dashboard,
-                R.id.navigation_notifications
-            )
-        )
-        setupActionBarWithNavController(navController, appBarConfiguration)
- */
+
+        setOnApplyWindowInsetsListener(findViewById(R.id.fragmentContainerView)) { view, insets ->
+            val windowInsets = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            view.updatePaddingRelative(top = windowInsets.top, bottom = windowInsets.bottom)
+            WindowInsetsCompat.CONSUMED
+        }
     }
 }
