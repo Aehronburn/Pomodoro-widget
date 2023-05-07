5db7cfc8a596196c549c9178cbe5a8f974d5b5fd
Alessandro Balzan <balzanalessandro2001@gmail.com>
2023-05-07 16:03:29 +0200

    Implemented menu for new session

--- app/src/main/java/it/unipd/dei/esp2023/sessions/CreateNewSessionDialog.kt
+++ app/src/main/java/it/unipd/dei/esp2023/sessions/CreateNewSessionDialog.kt
@@ -4,6 +4,9 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.fragment.app.DialogFragment
 import it.unipd.dei.esp2023.R
 
@@ -13,9 +16,28 @@ class CreateNewSessionDialog: DialogFragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(
+        val rootView: View = inflater.inflate(
             R.layout.fragment_dialog_create_new_session,
             container, false
         )
+
+        val cancelBtn: Button = rootView.findViewById<Button>(R.id.cancelBtn)
+        val submitBtn: Button = rootView.findViewById<Button>(R.id.submitBtn)
+
+        cancelBtn.setOnClickListener {
+            dismiss()
+        }
+
+        submitBtn.setOnClickListener {
+            val textInput = rootView.findViewById<View>(R.id.textInput) as EditText
+            val newName: String = textInput.getText().toString()
+            if (!newName.equals("")){
+                //TODO: Implement insertion into database
+                Toast.makeText(context, newName, Toast.LENGTH_LONG).show()
+                dismiss()
+            }
+        }
+
+        return rootView
     }
 }
