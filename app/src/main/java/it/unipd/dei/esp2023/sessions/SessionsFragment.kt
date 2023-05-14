5c8fee9718a094a21dbddd0fd62f1cbae0bbc14f
yihuizhu <yihuozhu@gmail.com>
2023-05-14 19:59:50 +0200

    removed old query from SessionsFragment

--- app/src/main/java/it/unipd/dei/esp2023/sessions/SessionsFragment.kt
+++ app/src/main/java/it/unipd/dei/esp2023/sessions/SessionsFragment.kt
@@ -1,33 +1,25 @@
 package it.unipd.dei.esp2023.sessions
 
-import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
-import androidx.fragment.app.DialogFragment
 import androidx.fragment.app.activityViewModels
-import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.floatingactionbutton.ExtendedFloatingActionButton
 import it.unipd.dei.esp2023.MainViewModel
 import it.unipd.dei.esp2023.R
-import kotlinx.coroutines.launch
 
 class SessionsFragment : Fragment() {
 
-    //private lateinit var viewModel: SessionsViewModel
 
-    //Pick the shared viewModel which is the same as in the MainActivity
     private val viewModel: MainViewModel by activityViewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        //viewModel = ViewModelProvider(this).get(SessionsViewModel::class.java)
 
         val view = inflater.inflate(R.layout.fragment_sessions, container, false)
 
@@ -38,12 +30,6 @@ class SessionsFragment : Fragment() {
         }
 
         val recyclerView = view.findViewById<RecyclerView>(R.id.recycler_id)
-
-        //Launch coroutine and pick data from database
-        //TODO: Make this change live
-        lifecycleScope.launch {
-            recyclerView.adapter = SessionsAdapter(viewModel.database.getSession("Hello"))
-        }
 
         recyclerView.setOnScrollChangeListener { v, scrollX, scrollY, oldScrollX, oldScrollY ->
             if(scrollY > oldScrollY) extendedFloatingActionButton.shrink()
