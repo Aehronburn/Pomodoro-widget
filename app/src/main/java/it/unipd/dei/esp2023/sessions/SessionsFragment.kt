b505dcc16b19ba4182780a42825eb3705bbc03b8
riccardodibella <riccardodibella2001@gmail.com>
2023-05-19 13:48:31 +0200

    Risolti problemi merge SessionsFragment

--- app/src/main/java/it/unipd/dei/esp2023/sessions/SessionsFragment.kt
+++ app/src/main/java/it/unipd/dei/esp2023/sessions/SessionsFragment.kt
@@ -4,10 +4,9 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.fragment.app.activityViewModels
+import androidx.fragment.app.viewModels
 import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.floatingactionbutton.ExtendedFloatingActionButton
-import it.unipd.dei.esp2023.MainViewModel
 import it.unipd.dei.esp2023.R
 import kotlinx.coroutines.cancel
 import kotlinx.coroutines.launch
@@ -24,12 +23,13 @@ import androidx.lifecycle.lifecycleScope
 import it.unipd.dei.esp2023.session_details.SessionDetailsFragment
 import kotlinx.coroutines.launch
 import android.util.Log
+import androidx.fragment.app.viewModels
 import it.unipd.dei.esp2023.database.Task
 
 class SessionsFragment : Fragment() {
 
 
-    private val viewModel: MainViewModel by activityViewModels()
+    private val viewModel: SessionsViewModel by viewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -39,14 +39,15 @@ class SessionsFragment : Fragment() {
         val view = inflater.inflate(R.layout.fragment_sessions, container, false)
 
         val extendedFloatingActionButton = view.findViewById<ExtendedFloatingActionButton>(R.id.create_new_session_fab)
-
         extendedFloatingActionButton.setOnClickListener{
             CreateNewSessionDialog().show(parentFragmentManager, "CreateNewSessionDialog")
         }
 
         val recyclerView = view.findViewById<RecyclerView>(R.id.recycler_id)
+        val adapter = SessionsAdapter(emptyList())
+        recyclerView.adapter = adapter
 
-        recyclerView.setOnScrollChangeListener { v, scrollX, scrollY, oldScrollX, oldScrollY ->
+        recyclerView.setOnScrollChangeListener { v, _, scrollY, _, oldScrollY ->
             if(scrollY > oldScrollY) extendedFloatingActionButton.shrink()
             else extendedFloatingActionButton.extend()
         }
