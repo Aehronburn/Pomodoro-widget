83373359e3f3dfffe00ddbf36e938028b607a8a5
yihuizhu <yihuozhu@gmail.com>
2023-05-21 15:04:32 +0200

    displays a toast requesting non empty name for creating a new session

--- app/src/main/java/it/unipd/dei/esp2023/sessions/CreateNewSessionDialog.kt
+++ app/src/main/java/it/unipd/dei/esp2023/sessions/CreateNewSessionDialog.kt
@@ -8,6 +8,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.Toast
 import androidx.core.widget.doOnTextChanged
 import androidx.fragment.app.DialogFragment
 import androidx.fragment.app.viewModels
@@ -53,6 +54,8 @@ class CreateNewSessionDialog: DialogFragment() {
         createButton.setOnClickListener {
             if(sessionName.isNotEmpty()) {
                 viewModel.insertSession(sessionName)
+            } else {
+                Toast.makeText(requireContext(), "Write a session name", Toast.LENGTH_SHORT).show()
             }
             dismiss()
         }
