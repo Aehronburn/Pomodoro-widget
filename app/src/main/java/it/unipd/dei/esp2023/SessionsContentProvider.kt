8e3382a91bff4708d2ff969e092575b994aa28a0
yihuizhu <yihuozhu@gmail.com>
2023-05-24 21:30:03 +0200

    Content provider query return the cursor obtained by calling a room dao. Other methods(apart from onCreate) throw UnsupportedOperationException

--- app/src/main/java/it/unipd/dei/esp2023/SessionsContentProvider.kt
+++ app/src/main/java/it/unipd/dei/esp2023/SessionsContentProvider.kt
@@ -3,47 +3,34 @@ package it.unipd.dei.esp2023
 import android.content.ContentProvider
 import android.content.ContentValues
 import android.database.Cursor
-import android.database.MatrixCursor
 import android.net.Uri
-import android.util.Log
 import it.unipd.dei.esp2023.database.PomodoroDatabase
 import it.unipd.dei.esp2023.database.PomodoroDatabaseDao
-import it.unipd.dei.esp2023.database.Session
 
 class SessionsContentProvider: ContentProvider() {
     lateinit var database: PomodoroDatabaseDao
 
     override fun onCreate(): Boolean {
-        //Pick database from application context because it already exists
         database = PomodoroDatabase.getInstance(context!!).databaseDao
         return true
     }
 
-    //Il cursore non è aggiornato autmaticamente, è solamente un'istantanea di
-    //un dato in quel preciso momento
     override fun query(
         uri: Uri,
         projection: Array<out String>?,
         selection: String?,
         selectionArgs: Array<out String>?,
         sortOrder: String?
-    ): Cursor? {
-        //TODO: Continua a pescare lista NULL da getSessionsList().value
-        //val myList = database.getSessionList()
-
-        val myList = database.getSessionListNoLive()
-
-        return createCursorFromList(myList)
+    ): Cursor {
+        return database.getSessionListCursor()
     }
 
     override fun insert(uri: Uri, values: ContentValues?): Uri? {
-        //This function is not available at the moment
-        return null
+        throw UnsupportedOperationException()
     }
 
     override fun delete(uri: Uri, selection: String?, selectionArgs: Array<out String>?): Int {
-        //This function is not available at the moment
-        return -1
+        throw UnsupportedOperationException()
     }
 
     override fun update(
@@ -52,24 +39,10 @@ class SessionsContentProvider: ContentProvider() {
         selection: String?,
         selectionArgs: Array<out String>?
     ): Int {
-        //This function is not available at the moment
-        return -1
+        throw UnsupportedOperationException()
     }
 
     override fun getType(uri: Uri): String? {
-        //This function is not available at the moment
-        return null
-    }
-
-    private fun createCursorFromList(myList: List<Session>): Cursor{
-        val cursor = MatrixCursor(arrayOf("id", "name", "creation_date"))
-            if(myList.isEmpty()){
-                Log.d("Il_mio_tag", "Lista vuota")
-            }
-
-            myList.forEach{
-                cursor.addRow(arrayOf(it.id, it.name, it.creationDate))
-            }
-     return cursor
+        throw UnsupportedOperationException()
     }
 }
