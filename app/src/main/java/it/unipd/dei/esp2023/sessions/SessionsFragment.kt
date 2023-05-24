03d2270141c1c7afcc9edd2213a22483c8b1b6b0
yihuizhu <yihuozhu@gmail.com>
2023-05-24 21:42:51 +0200

    updated test of ContentProvider inside SessionsFragment

--- app/src/main/java/it/unipd/dei/esp2023/sessions/SessionsFragment.kt
+++ app/src/main/java/it/unipd/dei/esp2023/sessions/SessionsFragment.kt
@@ -7,13 +7,11 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.core.content.contentValuesOf
 import androidx.fragment.app.viewModels
 import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.floatingactionbutton.ExtendedFloatingActionButton
 import it.unipd.dei.esp2023.R
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.LiveData
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.StaggeredGridLayoutManager
 import androidx.recyclerview.widget.StaggeredGridLayoutManager.VERTICAL
@@ -39,6 +37,7 @@ class SessionsFragment : Fragment() {
         }
 
         val recyclerView = view.findViewById<RecyclerView>(R.id.sessions_recyclerview)
+
         /*
         grid column count depends on the screen size. Default 1, for screen widths >= 400dp column count is 2
          */
@@ -60,16 +59,26 @@ class SessionsFragment : Fragment() {
         viewModel.sessionList.observe(viewLifecycleOwner) {
             adapter.updateList(it)
 
-            //Prova
+            //TODO temporary test of content provider
             val resolver: ContentResolver by lazy {
                 requireContext().contentResolver
             }
-            if(resolver!=null){
-                val uri = Uri.parse("content://it.unipd.dei.esp2023.SessionsContentProvider")
-                val cursor = resolver.query(uri, null, null, null, null)
-                val a = cursor!!.count
-                Log.d("Il_mio_tag", "Conto $a")
+            val uri = Uri.parse("content://it.unipd.dei.esp2023.SessionsContentProvider")
+            val cursor = resolver.query(uri, null, null, null, null)
+            cursor.use {
+                val count = cursor!!.count
+                Log.d("debug", "Conto $count")
+                Log.d("debug", cursor.columnNames.component1())
+                Log.d("debug", cursor.columnNames.component2())
+                Log.d("debug", cursor.columnNames.component3())
+                while (cursor.moveToNext()) {
+                    Log.d(
+                        "debug",
+                        cursor.getLong(0).toString() + cursor.getString(1) + cursor.getString(2)
+                    )
+                }
             }
+            //end
         }
 
         return view
