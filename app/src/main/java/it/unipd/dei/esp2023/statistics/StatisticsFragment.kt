fc3960495de06c01c447e4b15c5e2bedfc73fecf
yihuizhu <yihuozhu@gmail.com>
2023-06-02 12:18:58 +0200

    Statistics fragment uses databinding to update UI

--- app/src/main/java/it/unipd/dei/esp2023/statistics/StatisticsFragment.kt
+++ app/src/main/java/it/unipd/dei/esp2023/statistics/StatisticsFragment.kt
@@ -11,14 +11,12 @@ import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
 import com.patrykandpatrick.vico.core.axis.AxisPosition
 import com.patrykandpatrick.vico.core.axis.formatter.AxisValueFormatter
-import com.patrykandpatrick.vico.core.axis.formatter.DecimalFormatAxisValueFormatter
 import com.patrykandpatrick.vico.core.axis.horizontal.HorizontalAxis
 import com.patrykandpatrick.vico.core.axis.vertical.VerticalAxis
 import com.patrykandpatrick.vico.core.entry.FloatEntry
 import com.patrykandpatrick.vico.core.entry.entryModelOf
 import it.unipd.dei.esp2023.R
 import it.unipd.dei.esp2023.databinding.FragmentStatisticsBinding
-import java.math.RoundingMode
 
 class StatisticsFragment : Fragment() {
 
@@ -27,62 +25,68 @@ class StatisticsFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val binding: FragmentStatisticsBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_statistics, container, false)
+        val binding: FragmentStatisticsBinding =
+            DataBindingUtil.inflate(inflater, R.layout.fragment_statistics, container, false)
 
         binding.todayAction.setOnClickListener {
             findNavController().navigate(R.id.action_statistics_fragment_to_sessions_fragment)
         }
 
-
         /*
-        set x axis labels of week chart to week day
+        set x axis labels of week chart to week day (1, 2, 3, ...) -> ("Mon", "Tue", "Wed", ...)
          */
-        (binding.weekChart.bottomAxis as HorizontalAxis<AxisPosition.Horizontal.Bottom>).valueFormatter = weekDayAxisFormatter
-
-        /*
-        format y axis of chart with integers instead of floats
-        with(binding.weekChart.startAxis as VerticalAxis<AxisPosition.Vertical.Start>) {
-            //valueFormatter = startAxisFormatter
-            maxLabelCount = 7
-        }
-        with(binding.monthChart.startAxis as VerticalAxis<AxisPosition.Vertical.Start>) {
-            //valueFormatter = startAxisFormatter
-            maxLabelCount = 7
-        }
-
-         */
+        (binding.weekChart.bottomAxis as HorizontalAxis<AxisPosition.Horizontal.Bottom>).valueFormatter =
+            AxisValueFormatter { x, _ ->
+                listOf("Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun")[x.toInt() - 1]
+            }
 
         /*
         today's statistics
          */
         viewModel.todayStats.observe(viewLifecycleOwner) {
-            binding.todayPomodorosCompleted.text = getString(R.string.pomodoros_completed, it.numCompleted)
+            viewModel.setTodayCompleted(it.numCompleted)
+            viewModel.setTodayFocusTime(it.focusTime)
 
-            val (focusTimeHours, focusTimeMinutes) = timeToHhMm(it.focusTime)
-            binding.todayProductivityTime.text = getString(R.string.productivity_time, focusTimeHours, focusTimeMinutes)
-
-            val productivityImage = when(it.numCompleted) {
+            val productivityImage = when (it.numCompleted) {
                 0 -> R.drawable.pomodoro_artwork_sleeping
                 in 1..4 -> R.drawable.pomodoro_artwork_working
                 else -> R.drawable.pomodoro_artwork_completed
             }
-            binding.todayProductivityImage.setImageDrawable(ResourcesCompat.getDrawable(resources, productivityImage, context?.theme))
+            binding.todayProductivityImage.setImageDrawable(
+                ResourcesCompat.getDrawable(
+                    resources,
+                    productivityImage,
+                    context?.theme
+                )
+            )
         }
 
         /*
         week's statistics
          */
-        viewModel.weeksStats.observe(viewLifecycleOwner) {
-            val totalCompletedPomodoros = it.fold(0) { sum, singleStat ->  sum + singleStat.numCompleted}
-            binding.weekPomodorosCompleted.text = getString(R.string.pomodoros_completed, totalCompletedPomodoros)
+        viewModel.weekStats.observe(viewLifecycleOwner) {
+            val totalCompletedPomodoros =
+                it.fold(0) { sum, singleStat -> sum + singleStat.numCompleted }
+            viewModel.setWeekCompleted(totalCompletedPomodoros)
 
-            val totalFocusTime = it.fold(0) { sum, singleStat -> sum + singleStat.focusTime}
-            val (focusTimeHours, focusTimeMinutes) = timeToHhMm(totalFocusTime)
-            binding.weekProductivityTime.text = getString(R.string.productivity_time, focusTimeHours, focusTimeMinutes)
+            val totalFocusTime = it.fold(0) { sum, singleStat -> sum + singleStat.focusTime }
+            viewModel.setWeekFocusTime(totalFocusTime)
 
-            val eachDayCompletedPomodoros = it.map { singleStat -> FloatEntry(singleStat.dayNumber.toFloat(), singleStat.numCompleted.toFloat()) }
+            val eachDayCompletedPomodoros = it.map { singleStat ->
+                FloatEntry(
+                    singleStat.dayNumber.toFloat(),
+                    singleStat.numCompleted.toFloat()
+                )
+            }
             val entries = entryModelOf(eachDayCompletedPomodoros)
             binding.weekChart.setModel(entries)
+
+            /*
+            set number of y axis ticks to max value of the completed pomodoros in the period
+             */
+            val max = eachDayCompletedPomodoros.maxOfOrNull { entry -> entry.y.toInt() } ?: 0
+            (binding.weekChart.startAxis as VerticalAxis<AxisPosition.Vertical.Start>).maxLabelCount =
+                max
         }
 
         /*
@@ -91,13 +95,10 @@ class StatisticsFragment : Fragment() {
         viewModel.monthStats.observe(viewLifecycleOwner) {
             val totalCompletedPomodoros =
                 it.fold(0) { sum, singleStat -> sum + singleStat.numCompleted }
-            binding.monthPomodorosCompleted.text =
-                getString(R.string.pomodoros_completed, totalCompletedPomodoros)
+            viewModel.setMonthCompleted(totalCompletedPomodoros)
 
             val totalFocusTime = it.fold(0) { sum, singleStat -> sum + singleStat.focusTime }
-            val (focusTimeHours, focusTimeMinutes) = timeToHhMm(totalFocusTime)
-            binding.monthProductivityTime.text =
-                getString(R.string.productivity_time, focusTimeHours, focusTimeMinutes)
+            viewModel.setMonthFocusTime(totalFocusTime)
 
             val eachDayCompletedPomodoros = it.map { singleStat ->
                 FloatEntry(
@@ -107,35 +108,15 @@ class StatisticsFragment : Fragment() {
             }
             val entries = entryModelOf(eachDayCompletedPomodoros)
             binding.monthChart.setModel(entries)
+
+            val max = eachDayCompletedPomodoros.maxOfOrNull { entry -> entry.y.toInt() } ?: 0
+            (binding.weekChart.startAxis as VerticalAxis<AxisPosition.Vertical.Start>).maxLabelCount = max
         }
 
         binding.viewModel = viewModel
         binding.lifecycleOwner = viewLifecycleOwner
 
         return binding.root
-    }
-
-    companion object {
-
-        /*
-        Given time in minutes, returns the number of hours and number of minutes that do not make into an hour
-         */
-        private fun timeToHhMm(time: Int) : Pair<Int, Int> {
-            return Pair(time / 60, time % 60)
-        }
-
-        /*
-        associate week day in number returned by database to string: (1, 2, 3...) -> ("Mon", "Tue", "Wed"...)
-         */
-        private val weekDayAxisFormatter = AxisValueFormatter<AxisPosition.Horizontal.Bottom> { x, _ ->
-            listOf("Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun")[x.toInt() - 1]
-        }
-
-        /*
-        convert float y tick value to integer(pattern "#"), rounding to ceiling
-
-        private val startAxisFormatter = DecimalFormatAxisValueFormatter<AxisPosition.Vertical.Start>("#", RoundingMode.CEILING)
-         */
     }
 
 }
--- app/src/main/java/it/unipd/dei/esp2023/statistics/StatisticsViewModel.kt
+++ app/src/main/java/it/unipd/dei/esp2023/statistics/StatisticsViewModel.kt
@@ -2,6 +2,8 @@ package it.unipd.dei.esp2023.statistics
 
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import it.unipd.dei.esp2023.database.PomodoroDatabase
 import it.unipd.dei.esp2023.database.PomodoroDatabaseDao
 
@@ -15,8 +17,74 @@ class StatisticsViewModel(application: Application) : AndroidViewModel(applicati
 
     val todayStats = database.getTodayStatistics()
 
+    val weekStats = database.getCurrentWeekStatistics()
+
     val monthStats = database.getCurrentMonthStatistics()
 
-    val weeksStats = database.getCurrentWeekStatistics()
+    /*
+    today
+     */
+    private val _todayFocusTime = MutableLiveData<Pair<Int, Int>>(Pair(0, 0))
+    val todayFocusTime: LiveData<Pair<Int, Int>>
+        get() = _todayFocusTime
 
+    private val _todayCompleted = MutableLiveData<Int>(0)
+    val todayCompleted: LiveData<Int>
+        get() = _todayCompleted
+
+    /*
+    week
+     */
+    private val _weekFocusTime = MutableLiveData<Pair<Int, Int>>(Pair(0, 0))
+    val weekFocusTime: LiveData<Pair<Int, Int>>
+        get() = _weekFocusTime
+
+    private val _weekCompleted = MutableLiveData<Int>(0)
+    val weekCompleted: LiveData<Int>
+        get() = _weekCompleted
+
+    /*
+    month
+     */
+    private val _monthFocusTime = MutableLiveData<Pair<Int, Int>>(Pair(0, 0))
+    val monthFocusTime: LiveData<Pair<Int, Int>>
+        get() = _monthFocusTime
+
+    private val _monthCompleted = MutableLiveData<Int>(0)
+    val monthCompleted: LiveData<Int>
+        get() = _monthCompleted
+
+    //region setters
+
+    fun setTodayCompleted(completed: Int) {
+        _todayCompleted.value = completed
+    }
+    fun setTodayFocusTime(focusTime: Int) {
+        _todayFocusTime.value = timeToHhMm(focusTime)
+    }
+
+    fun setWeekCompleted(completed: Int) {
+        _weekCompleted.value = completed
+    }
+    fun setWeekFocusTime(focusTime: Int) {
+        _weekFocusTime.value = timeToHhMm(focusTime)
+    }
+
+    fun setMonthCompleted(completed: Int) {
+        _weekCompleted.value = completed
+    }
+    fun setMonthFocusTime(focusTime: Int) {
+        _weekFocusTime.value = timeToHhMm(focusTime)
+    }
+    //endregion
+
+    companion object {
+
+        /*
+        Given time in minutes, returns the number of hours and number of minutes that do not make into an hour
+         */
+        private fun timeToHhMm(time: Int) : Pair<Int, Int> {
+            return Pair(time / 60, time % 60)
+        }
+    }
 }
