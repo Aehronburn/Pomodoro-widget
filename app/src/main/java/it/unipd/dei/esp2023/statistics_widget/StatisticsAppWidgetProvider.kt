6c83c3ab2e47f53fc35e7d3ef3a94ef7ceda9210
yihuizhu <yihuozhu@gmail.com>
2023-06-06 22:17:52 +0200

    statistics widget for today, week and month. No responsive layout yet

--- app/src/main/java/it/unipd/dei/esp2023/statistics_widget/StatisticsAppWidgetProvider.kt
+++ app/src/main/java/it/unipd/dei/esp2023/statistics_widget/StatisticsAppWidgetProvider.kt
@@ -4,11 +4,7 @@ import android.appwidget.AppWidgetManager
 import android.appwidget.AppWidgetProvider
 import android.content.Context
 import android.net.Uri
-import android.os.Build
-import android.os.Bundle
-import android.util.SizeF
 import android.widget.RemoteViews
-import androidx.annotation.RequiresApi
 import it.unipd.dei.esp2023.R
 import it.unipd.dei.esp2023.content_providers.StatisticsContentProvider
 import it.unipd.dei.esp2023.statistics.StatisticsViewModel
@@ -18,6 +14,12 @@ class StatisticsAppWidgetProvider : AppWidgetProvider() {
     private var todayCompleted = 0
     private var todayFocusTime = 0
 
+    private var weekCompleted = 0
+    private var weekFocusTime = 0
+
+    private var monthCompleted = 0
+    private var monthFocusTime = 0
+
     override fun onUpdate(
         context: Context,
         appWidgetManager: AppWidgetManager,
@@ -26,6 +28,10 @@ class StatisticsAppWidgetProvider : AppWidgetProvider() {
         appWidgetIds.forEach { appWidgetId ->
 
             val contentResolver = context.contentResolver
+
+            /*
+            today stats
+             */
             val todayStats = contentResolver.query(
                 Uri.parse("content://" + StatisticsContentProvider.AUTHORITY + "/" + StatisticsContentProvider.TODAY_PATH),
                 null, null, null, null
@@ -33,19 +39,54 @@ class StatisticsAppWidgetProvider : AppWidgetProvider() {
             todayStats!!.moveToNext()
             todayCompleted = todayStats.getInt(2)
             todayFocusTime = todayStats.getInt(3)
-            val (todayFocusHours, todayFocusMinutes) = StatisticsViewModel.timeToHhMm(todayFocusTime)
             todayStats.close()
+            val (todayFocusHours, todayFocusMinutes) = StatisticsViewModel.timeToHhMm(todayFocusTime)
 
-            println("update called $todayFocusHours $todayFocusMinutes")
+            /*
+            week stats
+             */
+            val weekStats = contentResolver.query(
+                Uri.parse("content://" + StatisticsContentProvider.AUTHORITY + "/" + StatisticsContentProvider.WEEK_PATH),
+                null, null, null, null
+            )
+            while (weekStats!!.moveToNext()) {
+                weekCompleted += weekStats.getInt(2)
+                weekFocusTime += weekStats.getInt(3)
+            }
+            weekStats.close()
+            val (weekFocusHours, weekFocusMinutes) = StatisticsViewModel.timeToHhMm(weekFocusTime)
+
+
+            /*
+            month stats
+             */
+            val monthStats = contentResolver.query(
+                Uri.parse("content://" + StatisticsContentProvider.AUTHORITY + "/" + StatisticsContentProvider.MONTH_PATH),
+                null, null, null, null
+            )
+            while (monthStats!!.moveToNext()) {
+                monthCompleted += monthStats.getInt(2)
+                monthFocusTime += monthStats.getInt(3)
+            }
+            monthStats.close()
+            val (monthFocusHours, monthFocusMinutes) = StatisticsViewModel.timeToHhMm(monthFocusTime)
 
             val views = RemoteViews(context.packageName, R.layout.statistics_widget).apply {
                 setTextViewText(R.id.today_completed_text_widget, todayCompleted.toString())
-                setTextViewText(R.id.today_focus_time_text_widget, context.getString(R.string.stats_widget_today_focus_time, todayFocusHours, todayFocusMinutes))
+                setTextViewText(R.id.today_focus_time_text_widget, context.getString(R.string.stats_widget_focus_time, todayFocusHours, todayFocusMinutes))
+
+                setTextViewText(R.id.week_completed_text_widget, weekCompleted.toString())
+                setTextViewText(R.id.week_focus_time_text_widget, context.getString(R.string.stats_widget_focus_time, weekFocusHours, weekFocusMinutes))
+
+                setTextViewText(R.id.month_completed_text_widget, monthCompleted.toString())
+                setTextViewText(R.id.month_focus_time_text_widget, context.getString(R.string.stats_widget_focus_time, monthFocusHours, monthFocusMinutes))
             }
             appWidgetManager.updateAppWidget(appWidgetId, views)
         }
     }
 
+    /*
+    Just for printing device's cell sizes
     @RequiresApi(Build.VERSION_CODES.TIRAMISU)
     override fun onAppWidgetOptionsChanged(
         context: Context?,
@@ -64,6 +105,6 @@ class StatisticsAppWidgetProvider : AppWidgetProvider() {
             )
         )
         println(" ")
-
     }
+     */
 }
